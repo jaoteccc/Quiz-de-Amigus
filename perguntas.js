cc39fad6032ba970f050bb385febce82e70cafe7
jaoteccc <soprop5js@gmail.com>
2024-11-28 03:33:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Feriadp',
+    'Qual dia é o Natal',
+    '25 de Dezembro'
+)
+
+criaCartao(
+    'Geral',
+    'Quanto tempo a Terra demora para dar uma volta completa em torno dela mesma?',
+    'Aproximadamente 24 horas'
+)
+
+criaCartao(
+    'Geral',
+    'Qual é o maior planeta do sistema solar',
+    'jupiter'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a maior montanha da terra',
+    'monte everest'
+)
+
+criaCartao(
+    'Biologia',
+    'O que os pandas comem',
+    'bambu'
+)
+
+criaCartao(
+    'matematica',
+    'quanto é 1+1',
+    '2'
+)
